e43329c29249464da145ff8194a575e29aefe333
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 17:14:14 -0300

    logo

--- product-search-engine/src/Components/HagglesIcon.jsx
+++ product-search-engine/src/Components/HagglesIcon.jsx
@@ -1,8 +1,11 @@
-import logo from '../img/logo.svg'
+import logoDark from '../img/hagglesdark.png'
+import logoLight from '../img/haggleslight.png'
+
+import theme from '../style/theme'
 
 function HagglesIcon() {
   return (
-    <img src={logo} alt='' width='90px'/>
+    <img src={theme.palette.mode === 'dark'? logoDark : logoLight} alt='' width='150px'/>
   )
 }
 

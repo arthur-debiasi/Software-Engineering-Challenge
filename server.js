3ed54e442bd361ef7e883af47aafd5a95b81a2e1
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 12:17:17 -0300

    feat: conectando ao db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const searchProducts = require('./controller/searchProducts');
 const app = express();
 const port = 3001;
+const conn = require('./database/connection')
 app.use(cors());
 app.use(express.json());
 
@@ -12,7 +13,8 @@ app.get('/', (req, res) => {
 
 app.post('/', searchProducts);
 
-// Inicie o servidor
+conn();
+
 app.listen(port, () => {
   console.log(`A API está ouvindo na porta ${port}`);
 });

3438b3b9272028b817c2f32bde00c06520763275
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 12:19:28 -0300

    feat: buscape e meli Products validando e criando no DB

--- service/buscapeProducts.js
+++ service/buscapeProducts.js
@@ -1,5 +1,22 @@
 const buscapeScrapping = require('../buscapeScrapping');
+const QueryProducts = require('../database/Model/QueryProducts');
 
-const buscapeProducts = async (query, category) => await buscapeScrapping(query, category);
+const findQuery = async (query, category, web) => {
+  const foundQuery = await QueryProducts.findOne({query, category, web});
+  return foundQuery;
+}
+
+const buscapeProducts = async (query, category) => {
+  const haveBeenDone = await findQuery(query, category, 'buscape');
+  if(haveBeenDone) {console.log('haveBeenDone'); return haveBeenDone.products;}
+  const scrapping = await buscapeScrapping(query, category);
+  const results = await QueryProducts.create({
+    query,
+    category,
+    web: 'buscape',
+    products: scrapping,
+  });
+  return results.products; 
+};
 
 module.exports = buscapeProducts;
--- service/meliProducts.js
+++ service/meliProducts.js
@@ -1,5 +1,22 @@
+const QueryProducts = require('../database/Model/QueryProducts');
 const meliScrapping = require('../meliScrapping');
 
-const meliProducts = async (query, category) => await meliScrapping(query, category);
+const findQuery = async (query, category, web) => {
+  const foundQuery = await QueryProducts.findOne({query, category, web});
+  return foundQuery;
+}
+
+const meliProducts = async (query, category) => {
+  const haveBeenDone = await findQuery(query, category, 'meli');
+  if(haveBeenDone) {console.log('haveBeenDone'); return haveBeenDone.products;}
+  const scrapping = await meliScrapping(query, category);
+  const results = await QueryProducts.create({
+    query,
+    category,
+    web: 'meli',
+    products: scrapping,
+  });
+  return results.products; 
+};
 
 module.exports = meliProducts;

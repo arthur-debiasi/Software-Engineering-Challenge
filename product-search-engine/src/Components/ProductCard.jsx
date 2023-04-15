e2823b00e0635836fc8bde8d423665cf317df243
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 17:14:48 -0300

    alignment

--- product-search-engine/src/Components/ProductCard.jsx
+++ product-search-engine/src/Components/ProductCard.jsx
@@ -1,3 +1,5 @@
+import { Button, Stack, Typography } from '@mui/material';
+
 function ProductCard({ src, title, description, price, href }) {
 
   const handleClick = () => {
@@ -5,15 +7,17 @@ function ProductCard({ src, title, description, price, href }) {
   }
 
   return (
-    <div>
-      <img src={src} alt={title} width='200px' />
-      <div>
+    <Stack display='row' alignItems='center'>
+      <img src={src} alt={title} height='200px' />
+      <Stack maxWidth='500px'>
         <h3>{title}</h3>
-        <p>{description}</p>
+        <Stack>
+        <Typography>{description}</Typography>
+        </Stack>
         <h4>{price}</h4>
-        <button onClick={handleClick}>Ir para a web</button>
-      </div>
-    </div>
+        <Button onClick={handleClick}>Ir para a web</Button>
+      </Stack>
+    </Stack>
   );
 }
 

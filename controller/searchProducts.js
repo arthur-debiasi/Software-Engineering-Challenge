9b0ef8bf7750c857e2fcbad4d5def674ecba1218
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 12:17:46 -0300

    refat: lidando com erro

--- controller/searchProducts.js
+++ controller/searchProducts.js
@@ -1,28 +1,27 @@
+const allProducts = require('../service/allProducts');
 const buscapeProducts = require('../service/buscapeProducts');
 const meliProducts = require('../service/meliProducts');
 
 
 const searchProducts = async ({ body: { query, category, web } }, res) => {
   try {
-    let meli, buscape;
     switch (web) {
       case 'meli':
         console.log('meli');
-        meli = await meliProducts(query, category);
+        const meli = await meliProducts(query, category);
         return res.status(200).json(meli);
       case 'buscape':
         console.log('buscape');
-        buscape = await buscapeProducts(query, category);
+        const buscape = await buscapeProducts(query, category);
         return res.status(200).json(buscape);
       default:
         console.log('meli e buscape');
-        meli = await meliProducts(query, category);
-        buscape = await buscapeProducts(query, category);
-        const results = [...meli, ...buscape]
+        const results = await allProducts(query, category);
         return res.status(200).json(results);
     }
   } catch (error) {
-    console.error(error.message);
+    console.error(error);
+    res.status(500).json({message: 'Vixi... vixi... VIXI...'})
   }
 };
 

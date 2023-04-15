b3bc6f2cb716021c889f785e3711cbff66df6210
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-15 15:28:27 -0300

    aplicando mui

--- product-search-engine/src/Components/SearchBar.jsx
+++ product-search-engine/src/Components/SearchBar.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import NoQuery from './NoQuery';
+import { Button, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 function SearchBar({ handleChange, handleQueryBtn, noQuery }) {
   return (
-    <div>
-      <select name='web' onChange={handleChange}>
-        <option value='all'>Todas</option>
-        <option value='meli'>Mercado Livre</option>
-        <option value='buscape'>Buscapé</option>
-      </select>
+    <Stack direction='row' alignItems='center'>
 
-      <select name='category' onChange={handleChange}>
-        <option value='geladeira'>Geladeira</option>
-        <option value='tv'>TV</option>
-        <option value='celular'>Celular</option>
-      </select>
+      <FormControl sx={{ minWidth: '150px' }}>
+        <InputLabel id='web-label'>Web</InputLabel>
+        <Select
+          labelId='web-label'
+          id='web-select'
+          name='web'
+          label='Web'SearchIcon
+          onChange={handleChange}
+        >
+          <MenuItem value='all' defaultValue>
+            Todas
+          </MenuItem>
+          <MenuItem value='meli'>Mercado Livre</MenuItem>
+          <MenuItem value='buscape'>Buscape</MenuItem>
+        </Select>
+      </FormControl>
 
-      <input type='text' name='query' id='query' onChange={handleChange} />
+      <FormControl sx={{ minWidth: '150px' }}>
+        <InputLabel id='category-label'>Categoria</InputLabel>
+        <Select
+          name='category'
+          labelId='category-label'
+          id='category-select'
+          label='category'
+          onChange={handleChange}
+        >
+          <MenuItem value='geladeira' defaultValue>Geladeira</MenuItem>
+          <MenuItem value='tv'>TV</MenuItem>
+          <MenuItem value='celular'>Celular</MenuItem>
+        </Select>
+      </FormControl>
 
-      <button type='button' onClick={handleQueryBtn}>
-        Search
-      </button>
+      <TextField label='Pesquisa' type='text' name='query' id='query' onChange={handleChange} />
+
+      <Button type='button' onClick={handleQueryBtn}>
+        <SearchIcon />
+      </Button>
       {noQuery && <NoQuery />}
-    </div>
+    </Stack>
   );
 }
 

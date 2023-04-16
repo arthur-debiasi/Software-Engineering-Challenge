750036a4a21119299c99c2991ece67f3e5a853b5
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-16 11:41:35 -0300

    removendo função não usada

--- server/service/allProducts.js
+++ server/service/allProducts.js
@@ -1,16 +1,10 @@
-const QueryProducts = require('../database/Model/QueryProducts');
 const buscapeProducts = require('./buscapeProducts');
 const meliProducts = require('./meliProducts');
-
-const findQuery = async (query, category, web) => {
-  const foundQuery = await QueryProducts.findOne({query, category, web});
-  return foundQuery;
-}
 
 const allProducts = async (query, category) => {
   const meli = await meliProducts(query, category);
   const buscape = await buscapeProducts(query, category);
-  return [...meli, ...buscape]; 
+  return [...meli, ...buscape];
 };
 
 module.exports = allProducts;

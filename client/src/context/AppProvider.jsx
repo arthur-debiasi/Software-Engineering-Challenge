ac725761b8e3126e524ea80aac525cf19e1a0717
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-16 18:22:11 -0300

    aplicando provider

--- client/src/context/AppProvider.jsx
+++ client/src/context/AppProvider.jsx
@@ -1,11 +1,42 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import AppContext from './AppContext';
 
 export default function AppProvider({ children }) {
-  const teste = 'contexto';
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [noQuery, setNoQuery] = useState(false);
+  const [queryData, setQueryData] = useState({
+    query: '',
+    category: 'geladeira',
+    web: 'all',
+  });
 
-  const contextValue = useMemo(() => ({ teste }), [teste]);
+  const handleQueryBtn = useCallback(() => {
+    if (!queryData.query || queryData.query.replaceAll(' ', '') === '') {
+      setNoQuery(true);
+    } else {
+      setNoQuery(false);
+      setIsLoading(true);
+      const { query, category, web } = queryData;
+      axios
+        .post('http://localhost:3001/', { query, category, web })
+        .then(({ data }) => {
+          setProducts(data);
+          setIsLoading(false);
+        });
+    }
+  }, [queryData]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setQueryData({ ...queryData, [name]: value });
+  };
+
+  const contextValue = useMemo(() => ({
+    products, isLoading, noQuery, handleQueryBtn, handleChange,
+  }), [products, isLoading, noQuery, handleQueryBtn, handleChange]);
   return (
     <AppContext.Provider
       value={contextValue}
